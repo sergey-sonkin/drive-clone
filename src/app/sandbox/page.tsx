9ba3d8966a66d5afc7f46b231b19215e1606b8b3
Sergey Sonkin <mastersmail1@gmail.com>
2025-03-23 11:27:02 -0400

    Created a sandbox endpoint (🙈)

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,36 @@
+import { db } from "~/server/db";
+import { mockFiles, mockFolders } from "~/lib/mock-data";
+import { files, folders } from "~/server/db/schema";
+
+export default function SandboxPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      Seed Function
+      <form
+        action={async () => {
+          "use server";
+          const folderInsert = await db.insert(folders).values(
+            mockFolders.map((folder, index) => ({
+              id: index + 1,
+              name: folder.name,
+              parent: index !== 0 ? 1 : null,
+            })),
+          );
+          console.log(folderInsert);
+          const fileInsert = await db.insert(files).values(
+            mockFiles.map((file, index) => ({
+              id: index + 1,
+              name: file.name,
+              size: 5000,
+              url: file.url ?? "test.png",
+              parent: (index % 3) + 1,
+            })),
+          );
+          console.log(fileInsert);
+        }}
+      >
+        <button type="submit">Seed</button>
+      </form>
+    </div>
+  );
+}
